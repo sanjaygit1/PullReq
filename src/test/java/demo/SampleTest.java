2741352369e921f5ef8fd314a2c729a710e1da74
sanjaygit1 <rathinreddy1402@gmail.com>
2024-10-30 07:57:38 +0530

    pushing

---
+++ src/test/java/demo/SampleTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		//pull back
+		//pushBack
+		//Hi
+	}
+
+}
---
+++ src/test/java/demo/SimpleTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class SimpleTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Push1");
+		System.out.println("Pull1");
+		System.out.println("Push2");
+		System.out.println("Push to Branch1");
+	}
+
+}
